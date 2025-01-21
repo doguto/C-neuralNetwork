69c385f1d901df2c7c271e2c6ec6f13be9e7b20c
doguto <131944614+doguto@users.noreply.github.com>
2025-01-21 19:49:08 +0900

    [Feat] add newralNetwork struct

--- NewralNetwork.c
+++ NewralNetwork.c
@@ -3,3 +3,18 @@
 #include <string.h>
 #include <time.h>
 
+#define INPUT_AMOUNT 6
+#define HIDDEN_LAYER_AMOUNT 2
+#define HIDDEN_LAYER_SIZE 10
+
+typedef struct
+{
+    double input[6];
+    double hiddenParams[HIDDEN_LAYER_AMOUNT][HIDDEN_LAYER_SIZE];
+} NewralNetwork;
+
+/// 活性化関数
+double Activate(double x)
+{
+    
+}
